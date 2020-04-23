8ec99c690623004f4418f0a12379ff43ce426da5
mikeswhitney33 <mikeswhitney33@gmail.com>
2020-04-22 21:43:55 -0600

    pylintified the setup.py file

--- python/setup.py
+++ python/setup.py
@@ -1,42 +1,54 @@
-# Author: Vlad Morariu
-# Created: 2012-01-25
+"""
+Author: Vlad Morariu
+Created: 2012-01-25
+"""
+import sys
+import os
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
 import numpy
-import sys
-import os
+
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 FIGTREE_PATH = os.path.join(*os.path.split(CURRENT_DIR)[:-1])
 LIB_PATH = os.path.join(FIGTREE_PATH, "lib")
 INCLUDE_PATH = os.path.join(FIGTREE_PATH, "include")
 
-if not sys.platform == 'win32':
-   include_dirs=[INCLUDE_PATH, numpy.get_include()]
-   library_dirs=[LIB_PATH]
-   libraries=['figtree', 'ann_figtree_version']
-   extra_compile_args=['-DNDEBUG', '-O2']
-else:
-   include_dirs=[INCLUDE_PATH, numpy.get_include()]
-   library_dirs=[LIB_PATH]
-   libraries=['figtree', 'ann_figtree_version']
-   extra_compile_args=['-O2']
 
-ext_modules = [Extension(
-                         "figtree",
-                         ["figtree.pyx"],
-                         language="c++",
-                         include_dirs=include_dirs,
-                         library_dirs=library_dirs,
-                         libraries=libraries,
-                         extra_compile_args=extra_compile_args,
-                         extra_link_args=[])]
+def main():
+    """
+    performs the setup
+    """
+    if sys.platform != 'win32':
+        include_dirs = [INCLUDE_PATH, numpy.get_include()]
+        library_dirs = [LIB_PATH]
+        libraries = ['figtree', 'ann_figtree_version']
+        extra_compile_args = ['-DNDEBUG', '-O2']
+    else:
+        include_dirs = [INCLUDE_PATH, numpy.get_include()]
+        library_dirs = [LIB_PATH]
+        libraries = ['figtree', 'ann_figtree_version']
+        extra_compile_args = ['-O2']
 
-for e in ext_modules:
-   e.pyrex_directives={'embedsignature': True}
+    ext_modules = [Extension(
+        "figtree",
+        ["figtree.pyx"],
+        language="c++",
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        libraries=libraries,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=[])]
 
-setup(
-    ext_modules=ext_modules,
-    cmdclass={'build_ext' : build_ext}
-)
+    for ext in ext_modules:
+        ext.pyrex_directives = {'embedsignature': True}
+
+    setup(
+        ext_modules=ext_modules,
+        cmdclass={'build_ext' : build_ext})
+
+if __name__ == "__main__":
+    main()
